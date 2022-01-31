f585482d8898bd0bb94fc558f02ff1f38704c0b9
samar08 <samarasimhareddy.mohan@gmail.com>
2022-01-31 22:09:18 +0530

    git check 2

--- git_commands.sh
+++ git_commands.sh
@@ -1,4 +1,10 @@
+#install git commands
 #sudo apt-add-repository ppa:git-core/ppa
 #sudo apt-get update
 #sudo apt-get install git
-git --version
+#git --version
+
+# configure git commands
+git config --global user.name "samar08"
+git config --global user.email "samarasimhareddy.mohan@gmail.com"
+git config --global --list
