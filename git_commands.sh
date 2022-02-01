8f24f5c4997c75fc4ffc5ed4637cbbec8d1655fb
samar08 <samarasimhareddy.mohan@gmail.com>
2022-02-01 14:11:42 +0530

    git check 4

--- git_commands.sh
+++ git_commands.sh
@@ -4,13 +4,14 @@
 #sudo apt-get install git
 #git --version
 
-# configure git commands
+# configure git and git push commands
 git config --global user.name "samar08"
 git config --global user.email "samarasimhareddy.mohan@gmail.com"
 git config --global --list
 git clone https://github.com/samar08/lcm_argocd.git
 cd lcm_argocd
-ls
-echo "checking-samara" >> test.yaml
-ls
-cat test.yaml
+python updateImageVersion.py
+git add .
+git commit -m "updated image version ${METALLBVERSION}"
+git push origin
+cd ..
